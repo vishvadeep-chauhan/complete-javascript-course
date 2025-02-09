90010969afdd70a6f9d0182ae470e4a421fa66c9
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2025-02-10 00:33:46 +0530

    add

---
+++ 01-Fundamentals-Part-1/Assignment/02-Data-Types.js
@@ -0,0 +1,17 @@
+// 1. Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+
+// 2. Log the types of isIsland, population, country and language to the console.
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof language);
+
+// -------------------------------let, const and var
+
+// Set the value of language to the language spoken where you live (some countries have multiple languages, but just choose one).
+
+// Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.
+
+// Try to change one of the changed variables now, and observe what happens.
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -6,7 +6,7 @@ let firstName = "vishv";
 console.log(firstName);
 
 // let new = 27 ;
-let vishv_deep = name;
+let vishv_deep = "nameofUser";
 let $function = 26;
 
 let person = "deep";
@@ -14,3 +14,35 @@ let person = "deep";
 let PI = 3.2343;
 
 let myFirstJob = "Programmer";
+
+// -----------------------  data types --------------------
+//
+//  datatype defines what the kind of data , and variable storage of a value , javascript has two main types
+
+// primitive and object
+
+// primitive - stored by value
+let name = "vishv";
+
+// 1. number = floating point number used for decimals and integer
+let age = 23;
+
+// 2. string = sequence of characters , Used for text
+let UserName = "vishvadeep";
+
+//3. Boolean = Logical type that can only zero or one / true or false , used for taking decisions;
+let UserAge24 = true;
+
+//4. Undefined = Value taken by a variable that is not yet defined
+let children;
+
+// 5. Null = also mains "empty value"
+//6. Symbol (es2015) = value that is unique and that is can not be changed . ( not useful for now )
+//7. Bigint (es2020) = larger integer then the number can hold .
+
+// javascript dynamic typing language , it can automatic determine datatype of a value
+
+let javascriptIsTrue = true;
+console.log(javascriptIsTrue);
+
+console.log(typeof true);
