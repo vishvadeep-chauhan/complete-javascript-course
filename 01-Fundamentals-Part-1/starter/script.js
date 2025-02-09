e314ee1f0c3db557a1546a19853c671436f83888
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2025-02-09 18:49:37 +0530

    01-Values and Variables

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,16 @@
+let js = "amazing";
+console.log("vishvadeep");
+console.log(23);
+
+let firstName = "vishv";
+console.log(firstName);
+
+// let new = 27 ;
+let vishv_deep = name;
+let $function = 26;
+
+let person = "deep";
+
+let PI = 3.2343;
+
+let myFirstJob = "Programmer";
