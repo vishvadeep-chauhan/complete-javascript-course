fefcc22790f2054e83df6351062bfc9b0d727a46
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2025-02-13 02:41:54 +0530

    new pro

---
+++ js-project/project.js
@@ -0,0 +1,16 @@
+// 1. deposit some money
+// 2. determine number of lines to bet on
+// 3. collect a bet amount
+// 4. spin the slot machine
+// check if the user won
+// give the user their winnongs
+//play again
+
+// function deposit() {
+//     return 1;
+// }
+// const x = deposit()
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {};
